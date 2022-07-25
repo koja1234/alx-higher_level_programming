6bf01b102f3747d66ba26bfed5597f68ae86802f
Kassim Juma Amir <jumaamir352@gmail.com>
2022-07-25 13:31:24 -0700

    these are my tasks

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon July 25 2022
+@author: Kassim Juma Amir
+"""
+
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+    Attributes:
+        empty
+    """
+
+    pass
